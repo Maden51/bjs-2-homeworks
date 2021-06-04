ddd95c6ea337dabd4316c80da821cb31fd6ab150
Igor Modenov <madenon123@gmail.com>
2021-06-04 13:03:44 +0300

    Homework 2.functions added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,68 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = 101;
+  let max = -101;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  avg = (sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
-  // Ваш код
-
-  return sum;
+  return sum
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -1000;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumOfArr = func(arrOfArr[i]);
+    
+    if (sumOfArr > max) {
+      max = sumOfArr;
+    }
+  } 
+
   return max
+}
+// Задание 3
+
+function worker2(arr) {
+  let min = 101;
+  let max = -101;
+  let diff = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    
+    diff = max - min;
+  }
+
+  return diff
 }
