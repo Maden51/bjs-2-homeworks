b76b2e27d23b8a34474210af4ceafe66ea267cc9
Maden5126 <madenon123@gmail.com>
2021-06-27 23:01:37 +0300

    homework added

---
+++ 8.async/task.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+        this.time = 'time';
+        this.id = 'id';
+        this.callback = 'callback';
+    }
+
+    addClock(alarmTime, alarmCall, alarmId) {
+        if (alarmId === undefined) {
+            throw new Error('Необходимо указать идентификационный номер будильника');
+        } else {
+          if (this.alarmCollection.find(alarm => alarm[this.id] === alarmId)) {
+            console.error(`Такой будильник с идентификатором ${alarmId} уже существует`);
+            return false;
+            }
+          }
+        this.alarmCollection.push({[this.time]: alarmTime, [this.callback]: alarmCall, [this.id]: alarmId});
+        return true;
+    }
+     
+    removeClock(alarmId){
+        const oldLength = this.alarmCollection.length;
+        this.alarmCollection = this.alarmCollection.filter(item => item[this.id] !== alarmId)
+        return this.alarmCollection.length !== oldLength;
+    }
+
+    getCurrentFormattedTime(){
+        return new Date().toLocaleTimeString().substr(0,5);
+    }
+
+    addMinutesToCurrentFormattedTime(min) {
+        return new Date(Date.now() + min * 60000).toLocaleTimeString().substr(0,5);
+    }
+
+    start(){
+        const checkClock = (alarm) => {
+            if(alarm[this.time] === getCurrentFormattedTime()) {
+                alarm[this.callback]();
+            } 
+            if (this.timerId === null) {
+                this.timerId = setInterval(() => this.alarmCollection.forEach(item => checkClock(item)), 1000);
+            }
+        }   
+    }
+    
+    stop(){
+        if (this.timerId !== null) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    printAlarms(){
+        this.alarmCollection.forEach(item => console.log(`Будильник №${item.id} поставлен на ${item.time}`))
+    }
+
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
+
+function testCase() {
+    const phoneAlarm = new AlarmClock();
+
+    phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(), () => console.log("wake up"), 1);
+    phoneAlarm.addClock(phoneAlarm.addMinutesToCurrentFormattedTime(1), () => {console.log("wake up, Neo"); phoneAlarm.removeClock(2); }, 2);
+    phoneAlarm.addClock(phoneAlarm.addMinutesToCurrentFormattedTime(2), () => {console.log("follow white rabbit"); phoneAlarm.clearAlarms(); phoneAlarm.printAlarms(); }, 3);
+
+    phoneAlarm.printAlarms();
+    phoneAlarm.start();
+
+}
